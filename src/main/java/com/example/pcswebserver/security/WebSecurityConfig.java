25be498d5915b0f674b0fb7e85c3e13560478759
kivanval <kivanval@gmail.com>
2023-01-13 23:37:53 +0200

    Add Parent Access Inheriting

--- src/main/java/com/example/pcswebserver/security/WebSecurityConfig.java
+++ src/main/java/com/example/pcswebserver/security/WebSecurityConfig.java
@@ -1,24 +1,30 @@
 package com.example.pcswebserver.security;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static com.example.pcswebserver.web.WebConstants.AUTH;
 import static com.example.pcswebserver.web.WebConstants.STORE;
 
 @Configuration
 @EnableWebSecurity
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class WebSecurityConfig {
 
-    private JwtFilter jwtFilter;
+    JwtFilter jwtFilter;
+
+    AuthorizationManager<RequestAuthorizationContext> storeManager;
 
     @Bean
     public PasswordEncoder getEncoder() {
@@ -33,7 +39,7 @@ public class WebSecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers(STORE + "/**").access(new StoreManager())
+                .requestMatchers(STORE + "/**").access(storeManager)
                 .anyRequest().permitAll()
                 .and()
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
@@ -43,5 +49,10 @@ public class WebSecurityConfig {
     @Autowired
     public void setJwtFilter(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
+    }
+
+    @Autowired
+    public void setStoreManager(AuthorizationManager<RequestAuthorizationContext> storeManager) {
+        this.storeManager = storeManager;
     }
 }
